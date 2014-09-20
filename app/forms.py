4c88d827d3e0a42b1e6b3f29467a5e334df2adfe
Vedran Stankovic <email@vedranstankovic.com>
2014-09-20 21:11:13 +0200

    profile page and avatars

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,11 @@
 from flask.ext.wtf import Form
-from wtforms import TextField, BooleanField
-from wtforms.validators import DataRequired
+from wtforms import TextField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length
 
 class LoginForm(Form):
-    openid = TextField('openid', validators = [DataRequired()])
-    remember_me = BooleanField('remember_me', default = False)
+  openid = TextField('openid', validators = [DataRequired()])
+  remember_me = BooleanField('remember_me', default = False)
+
+class EditForm(Form):
+  nickname = TextField('nickname', validators = [DataRequired()])
+  about_me = TextAreaField('about_me', validators = [Length(min = 0, max = 140)])
--- app/models.py
+++ app/models.py
@@ -1,4 +1,5 @@
 from app import db
+from hashlib import md5
 
 ROLE_USER = 0
 ROLE_ADMIN = 1
@@ -6,9 +7,11 @@ ROLE_ADMIN = 1
 class User(db.Model):
   id = db.Column(db.Integer, primary_key = True)
   nickname = db.Column(db.String(64), unique = True)
-  email = db.Column(db.String(120), unique = True)
+  email = db.Column(db.String(120), index = True, unique = True)
   role = db.Column(db.SmallInteger, default = ROLE_USER)
   posts = db.relationship('Post', backref = 'author', lazy = 'dynamic')
+  about_me = db.Column(db.String(140))
+  last_seen = db.Column(db.DateTime)
 
   def is_authenticated(self):
     return True
@@ -24,6 +27,9 @@ class User(db.Model):
 
   def __repr__(self):
     return '<User %r>' % (self.nickname)
+
+  def avatar(self, size):
+    return 'http://www.gravatar.com/avatar/' + md5(self.email).hexdigest() + '?d=mm&s=' + str(size)
 
 class Post(db.Model):
   id = db.Column(db.Integer, primary_key = True)
