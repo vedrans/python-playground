150353588c78e596f98bfd4fe47fa3c5a1c44126
Vedran Stankovic <email@vedranstankovic.com>
2014-09-21 13:42:19 +0200

    followers, contacts, friends

--- app/forms.py
+++ app/forms.py
@@ -1,14 +1,14 @@
 from flask.ext.wtf import Form
 from wtforms import TextField, BooleanField, TextAreaField
-from wtforms.validators import Required, Length
+from wtforms.validators import DataRequired, Length
 from app.models import User
 
 class LoginForm(Form):
-  openid = TextField('openid', validators = [Required()])
+  openid = TextField('openid', validators = [DataRequired()])
   remember_me = BooleanField('remember_me', default = False)
 
 class EditForm(Form):
-  nickname = TextField('nickname', validators = [Required()])
+  nickname = TextField('nickname', validators = [DataRequired()])
   about_me = TextAreaField('about_me', validators = [Length(min = 0, max = 140)])
 
   def __init__(self, original_nickname, *args, **kwargs):
